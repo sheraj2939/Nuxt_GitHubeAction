ca0541fbeb23dbc1ebd0b26d75956ae966a826b2
sheraj Ahamad <sheraj.ahamad@houzeo.com>
2025-04-21 14:30:22 +0530

    added new pm2 file

---
+++ nuxt-app/ecosystem.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [
+    {
+      name: 'NuxtApp',
+      script: './.output/server/index.mjs',
+      exec_mode: 'fork',
+      watch: false,
+      instances: 1,
+    }
+  ]
+}
+
