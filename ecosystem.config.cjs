2158a1686c9ab95e24d021f36276dc2072598ed4
sheraj Ahamad <sheraj.ahamad@houzeo.com>
2025-04-21 16:14:30 +0530

    added new changes

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,16 +1,22 @@
 module.exports = {
   apps: [
     {
-      name: 'NuxtApp',
+      name: 'HouzeoFrontEndProd',
       script: './.output/server/index.mjs',
-      exec_mode: 'fork',  // Run in fork mode (single process)
-      watch: false,       // Disable watching for changes in production
-      instances: 1,       // Single instance (can be increased if needed)
+      exec_mode: 'fork',
+      instances: 1,
       env: {
-        PORT: 3000,       // Port on which the app will run
-        HOST: '0.0.0.0',  // Allow access on all interfaces (public IP)
+        NODE_ENV: 'production',
+        NITRO_PORT: 3000,
+        HOST: '0.0.0.0'
       },
+      error_file: '/data/logs/pm2/houzeo-frontend-prod-error.log',
+      out_file: '/data/logs/pm2/houzeo-frontend-prod-output.log',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss ZZ',
+      kill_timeout: 5000,
+      restart_delay: 2000,
+      autorestart: true
     }
   ]
-}
+};
 
