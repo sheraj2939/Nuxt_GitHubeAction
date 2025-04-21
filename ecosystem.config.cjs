0132416998373b483012e077453f9badf45cfa7f
sheraj Ahamad <sheraj.ahamad@houzeo.com>
2025-04-21 15:26:21 +0530

    change pm2 file

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,9 +3,13 @@ module.exports = {
     {
       name: 'NuxtApp',
       script: './.output/server/index.mjs',
-      exec_mode: 'fork',
-      watch: false,
-      instances: 1,
+      exec_mode: 'fork',  // Run in fork mode (single process)
+      watch: false,       // Disable watching for changes in production
+      instances: 1,       // Single instance (can be increased if needed)
+      env: {
+        PORT: 3000,       // Port on which the app will run
+        HOST: '0.0.0.0',  // Allow access on all interfaces (public IP)
+      },
     }
   ]
 }
